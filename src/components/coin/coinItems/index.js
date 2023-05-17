ed04fe96cbd67fbf0e565bdb845c8edc17346f1d
Mehrad <jafarifardz@gmail.com>
2023-05-17 16:41:11 +0330

    439

--- src/components/coin/coinItems/index.js
+++ src/components/coin/coinItems/index.js
@@ -214,7 +214,7 @@ const CoinItems = () => {
               <div className="text-red-700 font-semibold">فروش</div>
             </div>
             <div className="grid grid-cols-3 my-2 gap-x-2 ">
-              <div className="my-auto text-red-700 font-semibold">sdsdsd</div>
+              <div className="my-auto text-red-700 font-semibold">zzzzzzzz</div>
               <input
                 dir="ltr"
                 name="ghadimBuy"
