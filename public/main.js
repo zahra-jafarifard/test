2ed1ea0849838a8f3ea871342daffa6b0c45f7a0
Mehrad <jafarifardz@gmail.com>
2023-05-17 16:37:38 +0330

    437

--- public/main.js
+++ public/main.js
@@ -1,12 +1,20 @@
 const { app, BrowserWindow, Notification, ipcMain } = require("electron");
 const isDev = require("electron-is-dev");
 // const { autoUpdater } = require("electron-updater");
+const updater = require("electron-updater");
+const autoUpdater = updater.autoUpdater;
 
-require("update-electron-app")({
-  repo: "zahra-jafarifard/test",
-  updateInterval: "1 minute",
-  logger: require("electron-log"),
+
+
+
+autoUpdater.autoDownload = true;
+
+autoUpdater.setFeedURL({
+    provider: "generic",
+    url: "https://gitlab.com/_example_repo_/-/jobs/artifacts/master/raw/dist?job=build"
 });
+
+
 const path = require("path");
 
 function createWindow() {
@@ -46,6 +54,49 @@ app.on("window-all-closed", () => {
 ipcMain.handle("closeApp", () => {
   app.quit();
 });
+
+
+
+
+
+
+
+autoUpdater.on('checking-for-update', function () {
+  sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', function (info) {
+  sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', function (info) {
+  sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', function (err) {
+  sendStatusToWindow('Error in auto-updater.');
+});
+
+autoUpdater.on('download-progress', function (progressObj) {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+  sendStatusToWindow('Update downloaded; will install in 1 seconds');
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+  setTimeout(function () {
+      autoUpdater.quitAndInstall();
+  }, 1000);
+});
+
+autoUpdater.checkForUpdates();
+
+
 
 // app.on("ready", () => {
 //   autoUpdater.checkForUpdatesAndNotify();
