948a46ae609cd69f0e4d0c174f452e1e2b33813c
Mehrad <jafarifardz@gmail.com>
2023-05-17 14:56:03 +0330

    255 cmt

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
-const { app, BrowserWindow, Notification, ipcMain  } = require("electron");
+const { app, BrowserWindow, Notification, ipcMain } = require("electron");
 const isDev = require("electron-is-dev");
-const { autoUpdater } = require('electron-updater');
+const { autoUpdater } = require("electron-updater");
+
 const path = require("path");
+require("update-electron-app")({
+  repo: "zahta-jafarifard/test",
+  updateInterval: "1 minute",
+});
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -41,6 +46,32 @@ ipcMain.handle("closeApp", () => {
   app.quit();
 });
 
+mainWindow.once("ready-to-show", () => {
+  autoUpdater.checkForUpdatesAndNotify();
+  new Notification({
+    title: "پیام جدید ",
+    body: " aaa",
+    icon: path.join(__dirname, "success-icon.png"),
+  }).show();
+});
+
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+  new Notification({
+    title: "پیام جدید ",
+    body: " bbb",
+    icon: path.join(__dirname, "success-icon.png"),
+  }).show();
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+  new Notification({
+    title: "پیام جدید ",
+    body: " ccc",
+    icon: path.join(__dirname, "success-icon.png"),
+  }).show();
+});
+
 // var autoUpdater = require("auto-updater");
 
 const server = "https://vercel.com/zahra-jafarifard/test";
@@ -54,26 +85,55 @@ console.log("url: " + url);
 autoUpdater
   .on("error", function (err) {
     console.log(err);
+    new Notification({
+      title: "پیام جدید ",
+      body: " 3333",
+      icon: path.join(__dirname, "success-icon.png"),
+    }).show();
   })
   .on("checking-for-update", function () {
     console.log("Checking for update");
+    new Notification({
+      title: "پیام جدید ",
+      body: " 4444",
+      icon: path.join(__dirname, "success-icon.png"),
+    }).show();
   })
   .on("update-available", function () {
     console.log("Update available");
+    new Notification({
+      title: "پیام جدید ",
+      body: " 5555",
+      icon: path.join(__dirname, "success-icon.png"),
+    }).show();
   })
   .on("update-not-available", function () {
     console.log("Update not available");
+    new Notification({
+      title: "پیام جدید ",
+      body: " 6666",
+      icon: path.join(__dirname, "success-icon.png"),
+    }).show();
   })
   .on("update-downloaded", function () {
     console.log("Update downloaded");
+    new Notification({
+      title: "پیام جدید ",
+      body: " 7777",
+      icon: path.join(__dirname, "success-icon.png"),
+    }).show();
   });
 
 setInterval(() => {
   autoUpdater.checkForUpdates();
+  new Notification({
+    title: "پیام جدید ",
+    body: " 33aaaaaadddd33",
+    icon: path.join(__dirname, "success-icon.png"),
+  }).show();
 }, 10000);
 
-
-app.on('ready', function()  {
+app.on("ready", function () {
   autoUpdater.checkForUpdatesAndNotify();
 });
 // ipcMain.handle("openTelmisSite", () => {
