e92a5d478db1b4bd663f14142a98743af7826d37
Mehrad <jafarifardz@gmail.com>
2023-05-20 15:04:40 +0330

    304 new

--- public/dist/main.dev.js
+++ public/dist/main.dev.js
@@ -1,54 +1,56 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { autoUpdater } = require("electron-updater");
+"use strict";
 
-let mainWindow;
+var _require = require("electron"),
+    app = _require.app,
+    BrowserWindow = _require.BrowserWindow,
+    ipcMain = _require.ipcMain;
+
+var _require2 = require("electron-updater"),
+    autoUpdater = _require2.autoUpdater;
+
+var mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-    },
+      nodeIntegration: true
+    }
   });
   mainWindow.loadFile("public/index.html");
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
-
-  mainWindow.once("ready-to-show", () => {
+  mainWindow.once("ready-to-show", function () {
     autoUpdater.checkForUpdatesAndNotify();
   });
 }
 
-app.on("ready", () => {
+app.on("ready", function () {
   createWindow();
 });
-
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
 app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
-
-ipcMain.on("app_version", (event) => {
-  event.sender.send("app_version", { version: app.getVersion() });
+ipcMain.on("app_version", function (event) {
+  event.sender.send("app_version", {
+    version: app.getVersion()
+  });
 });
-
-autoUpdater.on("update-available", () => {
+autoUpdater.on("update-available", function () {
   mainWindow.webContents.send("update_available");
 });
-
-autoUpdater.on("update-downloaded", () => {
+autoUpdater.on("update-downloaded", function () {
   mainWindow.webContents.send("update_downloaded");
 });
-
-ipcMain.on("restart_app", () => {
+ipcMain.on("restart_app", function () {
   autoUpdater.quitAndInstall();
 });
