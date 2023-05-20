f80e737b710ca46db775923a4463bbe9f38d82e3
Mehrad <jafarifardz@gmail.com>
2023-05-20 16:28:58 +0330

    428 new

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       style={{ webkitAppRegion: "drag" }}
       className="App IRANSansWeb text-gray-700 rounded-3xl bg-[#c0c0c0]"
     >
-      <div>asdasa</div>
+      <div>asdasazzzz</div>
     </div>
   );
 }
